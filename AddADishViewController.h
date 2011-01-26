278c66d5ff19aead117bdf8aac457f3eb213244c
Roderic Campbell <roderic@topdish.com>
2011-01-25 23:23:15 -0800

    Initial run of AddADishViewController

---
+++ AddADishViewController.h
@@ -0,0 +1,16 @@
+//
+//  AddADishViewController.h
+//  TopDish
+//
+//  Created by roderic campbell on 1/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AddADishViewController : UITableViewController {
+
+}
+
+@end
