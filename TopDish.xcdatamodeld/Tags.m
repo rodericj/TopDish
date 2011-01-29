f15d92024f9645ea815febf2d6993451a4acef2e
Roderic Campbell <roderic@topdish.com>
2011-01-29 15:03:48 -0800

    Adding Tags for dish lookup

---
+++ TopDish.xcdatamodeld/Tags.m
@@ -0,0 +1,19 @@
+// 
+//  Tags.m
+//  TopDish
+//
+//  Created by roderic campbell on 1/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Tags.h"
+
+
+@implementation Tags 
+
+@dynamic id;
+@dynamic order;
+@dynamic name;
+@dynamic type;
+
+@end
